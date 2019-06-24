d69cbaf53a703ee9981b36fa210551c89b2f18b9
Jason DeCorte <jdecorte@decorteindustries.com>
2019-06-24 10:26:21 -0400

    Bumped dev version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with io.open('README.md', encoding='utf-8') as f:
 
 setup(
     name='pydruid',
-    version='0.5.4dev',
+    version='0.5.5dev',
     author='Druid Developers',
     author_email='druid-development@googlegroups.com',
     packages=['pydruid', 'pydruid.db', 'pydruid.utils'],
